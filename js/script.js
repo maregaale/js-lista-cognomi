4ccca04670e8958397d2163a444f1d7f8d29b668
Alessandro Marega <ale_marega@hotmail.it>
2021-03-03 15:14:48 +0100

    aggiunto inserimento cognome utente nell'array e stampa di quest'ultimo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // chiedo all'utente il cognome
 var cognomeUtente = prompt("Inserisci il tuo cognome");
+
+// creo array con lista cognomi
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+// inserisco il cognome dell'utente nell'array e stampo l'array
+listaCognomi.push(cognomeUtente);
+console.log(listaCognomi);

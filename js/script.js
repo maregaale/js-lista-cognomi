d27def65a53c462d13e6cb1a0cf1190c4da10442
Alessandro Marega <ale_marega@hotmail.it>
2021-03-03 16:17:20 +0100

    aggiunta variabile con maiuscola la prima lettera del nome utente

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // chiedo all'utente il cognome
 var cognomeUtente = prompt("Inserisci il tuo cognome");
+var cognomeUtenteCorretto = cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1);
 
 // creo array con lista cognomi
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
 // inserisco il cognome dell'utente nell'array e stampo l'array
-listaCognomi.push(cognomeUtente);
+listaCognomi.push(cognomeUtenteCorretto);
+
+// ordino la lista cognomi
+var listaOrdinata = listaCognomi.sort();
 
 // stampo la lista ordinata alfabeticamente
-var listaOrdinata = [];
-listaOrdinata = listaCognomi.sort();
-
-
 for (var i = 0; i < listaCognomi.length; i++) {
   document.getElementById('lista').innerHTML += "<li>" + listaOrdinata[i] + "</li>"
 }
